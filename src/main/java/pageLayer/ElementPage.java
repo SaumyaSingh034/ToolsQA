37a4567d9a8d14dbc05175da4721b92f2f935a99
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-09 12:30:04 +0530

    Automation is in progress

--- src/main/java/pageLayer/ElementPage.java
+++ src/main/java/pageLayer/ElementPage.java
@@ -1,4 +1,27 @@
 package pageLayer;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ElementPage {
+
+    private WebDriver driver;
+    TQA_DashboardPage dashboardPage;
+
+    private String heading;
+    private By header = By.id("Ad.Plus-970x250-1");
+    private By textBox = By.id("item-0");
+    private By checkbox = By.id("item-1");
+    private By radioBtn = By.id("item-2");
+    private By webTable = By.id("item-3");
+    private By butn = By.id("item-4");
+    private By link = By.id("item-5");
+    private By brokenLkn = By.id("item-6");
+    private By upload = By.id("item-7");
+    private By dynamicProperties = By.id("item-8");
+
+    public void getHeader(){
+        heading = driver.findElement(header).getText();
+    }
+
 }
--- src/test/java/stepDefinition/ElementsStepDef.java
+++ src/test/java/stepDefinition/ElementsStepDef.java
@@ -2,8 +2,11 @@ package stepDefinition;
 
 import factory.DriverFactory;
 import io.cucumber.java.en.And;
+import pageLayer.ElementPage;
 
 public class ElementsStepDef extends DriverFactory {
+    ElementPage elementPage;
+
 
     @And("user gets the heading of the page")
     public void userGetsTheHeadingOfThePage() {
